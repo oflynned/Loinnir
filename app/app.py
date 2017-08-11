2fcdff886a99060e8c27057d9038d48e493f0410
Ed <oflynned@tcd.ie>
2017-08-11 13:30:38 +0100

    modified app.py to try to fix mongo configs

--- app/app.py
+++ app/app.py
@@ -8,21 +8,13 @@ static_dir = os.path.abspath("../../Frontend/static/")
 app = Flask(__name__, template_folder=frontend_dir, static_folder=static_dir)
 app.config["MONGO_DBNAME"] = "loinnir"
 
-print(os.environ)
-
 if "MONGO_USERNAME" in os.environ:
-    print("using production")
-
     mongo_username = os.environ["MONGO_USERNAME"]
     mongo_password = os.environ["MONGO_PASSWORD"]
     mongo_url = os.environ["MONGO_URL"]
-    mongo_port = os.environ["MONGO_PORT"]
 
-    app.config["MONGO_URI"] = "mongodb://{username}:{password}@{url}:{port}/loinnir".format(
-        username=mongo_username, password=mongo_password, url=mongo_url, port=mongo_port)
+    app.config["MONGO_URI"] = "mongodb://" + mongo_username + ":" + mongo_password + "@" + mongo_url + "/loinnir"
 else:
-    print("using development")
-
     app.config["MONGO_URI"] = "mongodb://localhost:27017/loinnir"
     app.debug = True
 
