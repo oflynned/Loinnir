39b2992f6a8da65324e5e6caea1240d23d41a7b1
Ed <oflynned@tcd.ie>
2017-08-11 10:38:48 +0100

    change to run app without expressing port 80 on 0.0.0.0

--- Loinnir.py
+++ Loinnir.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         env = sys.argv[1]
         if env == "prod":
             mode = "prod"
-            app.run(host='0.0.0.0', port=80)
+            app.run()
 
     else:
         app.run(host='0.0.0.0', port=3000)
