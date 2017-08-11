9d1621c85196e06c597ca3d95b1870aa1ea6d0b1
Ed <oflynned@tcd.ie>
2017-08-11 13:00:58 +0100

    more sanity checks

--- app/app.py
+++ app/app.py
@@ -11,6 +11,8 @@ app.config["MONGO_DBNAME"] = "loinnir"
 print(os.environ)
 
 if "MONGO_USERNAME" in os.environ:
+    print("using production")
+
     mongo_username = os.environ["MONGO_USERNAME"]
     mongo_password = os.environ["MONGO_PASSWORD"]
     mongo_url = os.environ["MONGO_URL"]
@@ -19,6 +21,8 @@ if "MONGO_USERNAME" in os.environ:
     app.config["MONGO_URI"] = "mongodb://{username}:{password}@{url}:{port}/loinnir".format(
         username=mongo_username, password=mongo_password, url=mongo_url, port=mongo_port)
 else:
+    print("using development")
+
     app.config["MONGO_URI"] = "mongodb://localhost:27017/loinnir"
     app.debug = True
 
