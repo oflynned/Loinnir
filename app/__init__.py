3f2ce24b6e462da69532c2a932edffc86e5e346f
Ed <oflynned@tcd.ie>
2017-08-11 15:33:47 +0100

    fixed all authentication issues, was in app instead of __init__ >:(

--- app/__init__.py
+++ app/__init__.py
@@ -21,4 +21,17 @@ app.register_blueprint(user_endpoint, url_prefix="/api/v1/users")
 app.register_blueprint(messages_endpoint, url_prefix="/api/v1/messages")
 app.register_blueprint(services_endpoint, url_prefix="/api/v1/services")
 
+if "MONGO_USERNAME" in os.environ:
+    MONGO_HOST = str(os.environ["MONGO_URL"])
+    MONGO_PORT = int(os.environ["MONGO_PORT"])
+    MONGO_USERNAME = str(os.environ["MONGO_USERNAME"])
+    MONGO_PASSWORD = str(os.environ["MONGO_PASSWORD"])
+    MONGO_DBNAME = "loinnir"
+
+    app.config["MONGO_HOST"] = MONGO_HOST
+    app.config["MONGO_PORT"] = MONGO_PORT
+    app.config["MONGO_USERNAME"] = MONGO_USERNAME
+    app.config["MONGO_PASSWORD"] = MONGO_PASSWORD
+    app.config["MONGO_DBNAME"] = MONGO_DBNAME
+
 mongo = PyMongo(app)
--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
+import os
+
 from flask import Flask
 from flask_pymongo import PyMongo
-import os
 
 frontend_dir = os.path.abspath("../../Frontend/")
 static_dir = os.path.abspath("../../Frontend/static/")
@@ -8,14 +9,21 @@ static_dir = os.path.abspath("../../Frontend/static/")
 app = Flask(__name__, template_folder=frontend_dir, static_folder=static_dir)
 
 if "MONGO_USERNAME" in os.environ:
-    app.config["MONGO_URI"] = "mongodb://{username}:{password}@{host}:{port}/loinnir"\
-        .format(username=os.environ["MONGO_USERNAME"], password=os.environ["MONGO_PASSWORD"],
-                host=os.environ["MONGO_URL"], port=os.environ["MONGO_PORT"])
+    MONGO_HOST = str(os.environ["MONGO_URL"])
+    MONGO_PORT = int(os.environ["MONGO_PORT"])
+    MONGO_USERNAME = str(os.environ["MONGO_USERNAME"])
+    MONGO_PASSWORD = str(os.environ["MONGO_PASSWORD"])
+    MONGO_DBNAME = "loinnir"
 
-    print(app.config)
+    app.config["MONGO_URI"] = "mongodb://{username}:{password}@{host}:{port}/loinnir" \
+        .format(username=MONGO_USERNAME, password=MONGO_PASSWORD, host=MONGO_HOST, port=MONGO_PORT)
+
+    print("using production")
 
 else:
     app.config["MONGO_URI"] = "mongodb://localhost:27017/loinnir"
     app.debug = True
+
+    print("using development")
 
 mongo = PyMongo(app)
