6ff989a8a0fe60d314f39ae2fbbd48df8b4eb4aa
Ed <oflynned@tcd.ie>
2017-08-11 13:41:53 +0100

    update again config vals

--- app/app.py
+++ app/app.py
@@ -9,11 +9,11 @@ app = Flask(__name__, template_folder=frontend_dir, static_folder=static_dir)
 app.config["MONGO_DBNAME"] = "loinnir"
 
 if "MONGO_USERNAME" in os.environ:
-    mongo_username = os.environ["MONGO_USERNAME"]
-    mongo_password = os.environ["MONGO_PASSWORD"]
-    mongo_url = os.environ["MONGO_URL"]
+    app.config["MONGO_USERNAME"] = os.environ["MONGO_USERNAME"]
+    app.config["MONGO_PASSWORD"] = os.environ["MONGO_PASSWORD"]
+    app.config["MONGO_HOST"] = os.environ["MONGO_URL"]
+    app.config["MONGO_PORT"] = os.environ["MONGO_PORT"]
 
-    app.config["MONGO_URI"] = "mongodb://" + mongo_username + ":" + mongo_password + "@" + mongo_url + "/loinnir"
 else:
     app.config["MONGO_URI"] = "mongodb://localhost:27017/loinnir"
     app.debug = True
