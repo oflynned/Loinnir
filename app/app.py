3bac2cc20d20862af4cbb1fc12047facd8d6ceb2
Ed <oflynned@tcd.ie>
2017-08-11 14:38:08 +0100

    revert last commit

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ static_dir = os.path.abspath("../../Frontend/static/")
 
 app = Flask(__name__, template_folder=frontend_dir, static_folder=static_dir)
 
-if "MONGO_URI" in os.environ:
+if "MONGO_USERNAME" in os.environ:
     app.config["MONGO_URI"] = "mongodb://{username}:{password}@{host}:{port}/loinnir"\
         .format(username=os.environ["MONGO_USERNAME"], password=os.environ["MONGO_PASSWORD"],
                 host=os.environ["MONGO_URL"], port=os.environ["MONGO_PORT"])
