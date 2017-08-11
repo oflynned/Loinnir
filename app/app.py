63624f07eaae37097151877b8cbfe9cbcf7629a8
Ed <oflynned@tcd.ie>
2017-08-11 12:43:49 +0100

    fixes to change to environmental vars for easier configs

--- app/app.py
+++ app/app.py
@@ -9,12 +9,13 @@ app = Flask(__name__, template_folder=frontend_dir, static_folder=static_dir)
 app.config["MONGO_DBNAME"] = "loinnir"
 
 if "MONGO_USERNAME" in os.environ and "MONGO_PASSWORD" in os.environ:
-    username = os.environ["MONGO_USERNAME"]
-    password = os.environ["MONGO_PASSWORD"]
-    url = "52.19.192.229"
-    port = 27017
+    mongo_username = os.environ["MONGO_USERNAME"]
+    mongo_password = os.environ["MONGO_PASSWORD"]
+    mongo_url = os.environ["MONGO_URL"]
+    mongo_port = os.environ["MONGO_PORT"]
+
     app.config["MONGO_URI"] = "mongodb://{username}:{password}@{url}:{port}/loinnir".format(
-        username=username, password=password, url=url, port=port)
+        username=mongo_username, password=mongo_password, url=mongo_url, port=mongo_port)
 else:
     app.config["MONGO_URI"] = "mongodb://localhost:27017/loinnir"
     app.debug = True
