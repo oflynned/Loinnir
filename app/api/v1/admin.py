49c0852d39bd02900708810811368df2499c979d
Ed <oflynned@tcd.ie>
2017-09-05 13:02:58 +0100

    finished current iteration of coarse user statistics for aggregating platform use. need to generate a web interface through a web app

--- app/api/v1/admin.py
+++ app/api/v1/admin.py
@@ -46,8 +46,17 @@ def get_message_stats():
             {"time": {"$gt": get_time_24_hours_ago()}}).count()
         locality_message_count_24_hours = mongo.db.locality_conversations.find(
             {"time": {"$gt": get_time_24_hours_ago()}}).count()
+
         partner_message_count = mongo.db.partner_conversations.find().count()
         locality_message_count = mongo.db.locality_conversations.find().count()
+
+        user_count = mongo.db.users.find().count()
+        users_active_last_24_hours = mongo.db.users.find({"last_active": {"$gt": get_time_24_hours_ago()}}).count()
+
+        users_who_enacted_blocks = list(mongo.db.users.find({"blocked": {"$ne": []}}))
+        user_block_count = 0
+        for user in users_who_enacted_blocks:
+            user_block_count += len(user["blocked"])
 
         return Helper.get_json(
             {
@@ -57,6 +66,9 @@ def get_message_stats():
                 "partner_message_count_24_hours": partner_message_count_24_hours,
                 "locality_message_count_24_hours": locality_message_count_24_hours,
                 "total_message_count_24_hours": partner_message_count_24_hours + locality_message_count_24_hours,
+                "user_count": user_count,
+                "users_active_last_24_hours": users_active_last_24_hours,
+                "user_block_count": user_block_count,
                 "time_24_hours_ago": Helper.get_current_time_in_millis() - get_time_24_hours_ago()
             }
         )
@@ -92,8 +104,44 @@ def get_locality_messages_last_24_hours():
 @admin_endpoint.route("/get-all-locality-conversations", methods=["POST"])
 def get_all_locality_conversations():
     if authenticate_user(request.json):
-        count = mongo.db.locality_conversations.find().count()
-        return Helper.get_json({"count": count})
+        messages = list(mongo.db.locality_conversations.find())
+        localities = []
+        output = {}
+
+        for message in messages:
+            locality = parse.unquote_plus(message["locality"])
+            if locality not in localities and len(locality) > 0:
+                localities.append(locality)
+
+        for locality in localities:
+            output[locality] = []
+
+        for message in messages:
+            for locality in localities:
+                if parse.unquote_plus(message["locality"]) == locality:
+                    output[locality].append(message)
+
+        return Helper.get_json(output)
+
+    return Helper.get_json({"success": False})
+
+
+# TODO may not be necessary now but could be useful for ML grooming of learning about user interaction
+# TODO or also with user blocks/admin intervention -- currently unimplemented
+# TODO may also be a breach of privacy
+@admin_endpoint.route("/get-all-partner-conversations", methods=["POST"])
+def get_all_partner_conversations():
+    if authenticate_user(request.json):
+        messages = list(mongo.db.partner_conversations.find())
+        match_ids = []
+        output = {}
+
+        for message in messages:
+            conversation_couple = [message["from_id"], message["to_id"]]
+            if conversation_couple not in match_ids:
+                match_ids.append(conversation_couple)
+
+        return Helper.get_json(match_ids)
 
     return Helper.get_json({"success": False})
 
